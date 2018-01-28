46f7a3e3fc6f44b206dea0757bb45a56331e582e
LEONYX\Leonyx <leonyx03@gmail.com>
2018-01-28 13:42:25 -0600

    Updated level with spawn locations, teleport locations, updated enemy paths and gate removal.

---
+++ Project Mouth Voice/Assets/Scripts/Switch/OpenGateSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGateSwitch : Switch {
+
+	[SerializeField] private List<GameObject> toRemove;
+
+	protected override void SwitchEffect() {
+		base.SwitchEffect();
+
+		foreach(GameObject thing in toRemove) {
+			Destroy(thing);
+		}
+
+		EndSwitch();
+	}
+
+}
